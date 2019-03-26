4b5b16324c5c690954de30d506f8ee70c7632314
naomital <naomilau699@gmail.com>
2019-03-26 21:18:23 +0200

    new testing- bigtree and treeright

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -13,35 +13,59 @@ using std::cout, std::endl;
 int main() {
   ariel::Tree emptytree;
   ariel::Tree threetree;
+  ariel::Tree randomali;
   ariel::Tree ourtree;
-  ariel::Tree ourtree1;
-  ariel::Tree mytree;
-
+  ariel::Tree treeright;
+  ariel::Tree bigtree;
+//insert threetree
   threetree.insert(5);
   threetree.insert(7);
   threetree.insert(3);
-
+//insert ourtre
 int random[5] = {rand()%100,rand()%100,rand()%100,rand()%100,rand()%100};
+   for(int i =0;i<5;i++){
+   randomali.insert(random[i]);
+   }
+//insert ourtre1
 int arr[5] = {7,4,3,2,5};
 for(int i =0;i<5;i++){
-ourtree1.insert(arr[i]);
+ourtree.insert(arr[i]);
 }
-   for(int i =0;i<5;i++){
-   ourtree.insert(random[i]);
-   }
+//insert treeright
+treeright.insert(8);
+treeright.insert(9);
+treeright.insert(10);
+treeright.insert(11);
+treeright.insert(12);
 
+// insert bigtree
 
+bigtree.insert(5);
+bigtree.insert(3);
+bigtree.insert(8);
+bigtree.insert(4);
+bigtree.insert(10);
+bigtree.insert(7);
+bigtree.insert(6);
+bigtree.insert(2);
+bigtree.insert(1);
+bigtree.insert(9);
+bigtree.insert(12);
+
+//testing
+
+//emptytree
   badkan::TestCase tc("Binary tree");
   tc
   .CHECK_EQUAL (emptytree.size(), 0)
   .CHECK_OK    (emptytree.insert(5))
   .CHECK_EQUAL (emptytree.size(), 1)
   .CHECK_EQUAL (emptytree.contains(5), true)
- .CHECK_OK    (emptytree.remove(5))
+  .CHECK_OK (emptytree.remove(5))
   .CHECK_EQUAL (emptytree.contains(5), false)
-  .CHECK_THROWS(emptytree.remove(5))
+  //.CHECK_THROWS(emptytree.remove(5))
   .CHECK_EQUAL (emptytree.size() ,0)
-
+//threetree
   .CHECK_EQUAL (threetree.size(), 3)
   .CHECK_EQUAL (threetree.root(), 5)
   .CHECK_EQUAL (threetree.parent(3), 5)
@@ -52,24 +76,87 @@ ourtree1.insert(arr[i]);
   .CHECK_THROWS(threetree.left(6))
   .CHECK_OK    (threetree.print())
   .print();
-
+//randomali
     for(int i =0;i<5;i++){
-   tc.CHECK_EQUAL (ourtree.contains(random[i]),true);
+   tc.CHECK_EQUAL (randomali.contains(random[i]),true);
    }
-   tc.CHECK_EQUAL (ourtree.size() ,5);
-
-   tc.CHECK_EQUAL (ourtree1.parent(3), 4)
-   .CHECK_EQUAL (ourtree1.left(3), 2)
-     .CHECK_EQUAL (ourtree1.right(3), NULL);
-
-
-
+   tc.CHECK_EQUAL (randomali.size() ,5)
+   .CHECK_EQUAL (randomali.parent(random[0]), NULL).print();
+   
     for(int i =0;i<5;i++){
-    tc.CHECK_THROWS(ourtree.remove(random[i]));
-    tc.CHECK_EQUAL (ourtree.size(), 5-i);
+    tc.CHECK_THROWS(randomali.remove(random[i]));
+    tc.CHECK_EQUAL (randomali.size(), 5-i);
    }
 
+//ourtree
+   tc.CHECK_EQUAL (ourtree.parent(3), 4)
+   .CHECK_EQUAL (ourtree.left(3), 2)
+   .CHECK_EQUAL (ourtree.right(3), NULL).print();
 
+//treeright
+for(int i =8;i<13;i++){
+   tc.CHECK_EQUAL (treeright.contains(i),true);
+   }
+  tc.CHECK_EQUAL (treeright.size(), 5)
+    .CHECK_THROWS(treeright.left(8))
+    .CHECK_THROWS(treeright.left(9))
+    .CHECK_THROWS(treeright.left(10))
+    .CHECK_THROWS(treeright.left(11))
+    .CHECK_THROWS(treeright.left(12))
+    .CHECK_EQUAL (treeright.right(8), 9)
+    .CHECK_EQUAL (treeright.right(9), 10)
+    .CHECK_EQUAL (treeright.right(10), 11)
+    .CHECK_EQUAL (treeright.right(11), 12)
+    .CHECK_OK  (treeright.remove(10))
+    .CHECK_THROWS(treeright.remove(10))
+    .CHECK_EQUAL (treeright.size(), 4)
+    .CHECK_OK    (treeright.insert(10))
+    .CHECK_THROWS(treeright.insert(10))
+    .CHECK_EQUAL (treeright.parent(10), 11)
+    .CHECK_EQUAL (treeright.left(11), 10)
+    .CHECK_EQUAL (treeright.right(11), 12)
+    .CHECK_THROWS(treeright.remove(0)).print();
+  for(int i =8;i<13;i++){
+    tc.CHECK_THROWS(treeright.remove(i));
+    tc.CHECK_EQUAL (treeright.size(), 5-i);
+   }
+
+ //bigtree
+   tc.CHECK_EQUAL (bigtree.right(5), 8)
+         .CHECK_EQUAL (bigtree.right(8), 10)
+         .CHECK_EQUAL (bigtree.right(10), 12)
+         .CHECK_EQUAL (bigtree.right(3), 4)
+         .CHECK_EQUAL (bigtree.left(5), 3)
+         .CHECK_EQUAL (bigtree.left(3), 2)
+         .CHECK_EQUAL (bigtree.left(2), 1)
+         .CHECK_EQUAL (bigtree.left(8), 7)
+         .CHECK_EQUAL (bigtree.left(7), 6)
+         .CHECK_EQUAL (bigtree.left(10), 9)
+         .CHECK_THROWS(bigtree.left(4))
+         .CHECK_THROWS(bigtree.left(6))
+         .CHECK_THROWS(bigtree.left(9))
+         .CHECK_THROWS(bigtree.left(12))
+         .CHECK_THROWS(bigtree.left(1))
+         .CHECK_THROWS(bigtree.right(4))
+         .CHECK_THROWS(bigtree.right(7))
+         .CHECK_THROWS(bigtree.right(6))
+         .CHECK_THROWS(bigtree.right(4))
+         .CHECK_THROWS(bigtree.right(9))
+         .CHECK_THROWS(bigtree.right(12))
+         .CHECK_THROWS(bigtree.right(2))
+         .CHECK_THROWS(bigtree.right(1))
+         .CHECK_EQUAL (bigtree.size(),11)
+         .print();
+for(int i =1;i<11;i++){
+   tc.CHECK_EQUAL (bigtree.contains(i),true);
+   }
+ tc.CHECK_EQUAL (bigtree.contains(12),true);
+ for(int i =1;i<11;i++){
+    tc.CHECK_THROWS(bigtree.remove(i));
+    tc.CHECK_EQUAL (bigtree.size(), 11-i);
+   }
+ tc.CHECK_THROWS(bigtree.remove(12));
+    tc.CHECK_EQUAL (bigtree.size(), 0);
 
 
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
